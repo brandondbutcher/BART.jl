f100412bbebf835fb84f33b59cb507e402573c2d
brandondbutcher <brandondbutcher@gmail.com>
2020-02-04 19:28:59 -0600

    Fix typo.

--- src/proposals.jl
+++ src/proposals.jl
@@ -404,7 +404,7 @@ function mpost(c)
   M = reduce(hcat, [leafprob(bm.td.X, tree) for tree in trees])
   sig = [1 + bm.hypers.τ*dot(M[i,:], M[i,:]) for i in 1:size(M, 1)]
   ym = sum(log.(cdf.(Normal.(0, sig), 0)))
-  ltp = sum([log_tree_prior(tree, post.bm) for tree in trees])
+  ltp = sum([log_tree_prior(tree, bm) for tree in trees])
   ym + ltp
 end
 
